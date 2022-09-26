0d7cc695dda88e62971fa756ce9bee7026415254
Rony <rony.ahmed9771@gmail.com>
2022-09-27 00:46:54 +0600

    Add country name & flag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCoutries></LoadCoutries>
+      
     </div>
   );
+}
+
+function LoadCoutries(){
+
+  const [countries, setCountries] =useState([]);
+
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  },[])
+  return (
+    <div >
+      <h1>Welcome to World Tour with React!!!</h1>
+      <p>Total Country {countries.length}</p>
+      {
+        countries.map(country => <Country name={country.name.common} flag={country.flags.png}></Country>)
+      }
+    </div>
+  )
+
+  function Country(props){
+    return (
+      <div style={{border:'3px solid green', borderradius:'20px', margin:'15px'}}>
+        <h3> Name : {props.name}</h3>
+        <img src={props.flag} alt="" />
+      </div>
+    )
+
+  }
 }
 
 export default App;
